0aa6c054aa45022f6ef1114af938e0c2a5786f48
Dave Brown <whis101ismyname@gmail.com>
2025-06-17 19:00:24 +0000

    added routes and gitignore file

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# More lines of code
+# More lines of code
+@app.route('/',methods=['GET'])
+def hello_world():
+    return "Hello World!\n"
+
+@app.route('/<name>',methods=['GET'])
+def hello_name(name):
+    return f'Hello, {name}!\n'
+
+# last line of application
+if __name__ == '__main__':
+    app.run(debug=True, host ='0.0.0.0',port=8021)
